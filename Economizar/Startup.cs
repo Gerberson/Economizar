3f4d8739cac48fd3a1ce4daeb67f16d377a34a38
Gerberson Dias <gerbersondias.312@gmail.com>
2019-08-15 12:59:05 -0300

    Adicionar arquivos de projeto.

---
+++ Economizar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Economizar.Startup))]
+namespace Economizar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
