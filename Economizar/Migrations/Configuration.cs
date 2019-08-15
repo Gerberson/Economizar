fb2c2152b74ee81bc7e91633781a02f19e4d7f94
Gerberson Dias <gerbersondias.312@gmail.com>
2019-08-15 13:28:42 -0300

    Configrando o projeto para migrassoes automaticas

---
+++ Economizar/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace Economizar.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Economizar.Models.EconomizarContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Economizar.Models.EconomizarContext";
+        }
+
+        protected override void Seed(Economizar.Models.EconomizarContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
